3a02aa646bb7b376f88170bcb7677608c30d86ef
Nakul Singh <NakulS426@gmail.com>
2021-01-11 12:33:07 +0530

    file name changed added some more Endpoints
    Signed-off-by: Nakul Singh <NakulS426@gmail.com>

---
+++ app/src/main/java/com/ig/igdownloader/nakul/Endpoints.java
@@ -0,0 +1,12 @@
+package com.ig.igdownloader.nakul;
+
+public final class Endpoints {
+    public static final String INSTAGRAM_URL = "https://www.instagram.com/";
+    public static final String INSTAGRAM_ENDPOINT = "?__a=1";
+    public static final String INSTAGRAM_HD_PIC_ENDPOINT = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/profile";
+    public static final String INSTAGRAM_USER_ID = "?userid=";
+    public static final String INSTAGRAM_VIDEOS_ENDPOINT = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/video";
+    public static final String INSTAGRAM_VIDEO_PATH = "?link=";
+    public static final String INSTAGRAM_SEARCH_USER = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/topsearch";
+    public static final String INSTAGRAM_QUERY_USER = "?query=";
+}
