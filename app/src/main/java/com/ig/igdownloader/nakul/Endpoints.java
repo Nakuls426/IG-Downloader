9149240ae9c57f3f06ffdc2e964f23e86703b054
Nakul Singh <nakuls426@gmail.com>
2021-02-08 12:32:30 +0530

    API updated
    Signed-off-by: Nakul Singh <NakulS426@gmail.com>

--- app/src/main/java/com/ig/igdownloader/nakul/Endpoints.java
+++ app/src/main/java/com/ig/igdownloader/nakul/Endpoints.java
@@ -3,10 +3,10 @@ package com.ig.igdownloader.nakul;
 public final class Endpoints {
     public static final String INSTAGRAM_URL = "https://www.instagram.com/";
     public static final String INSTAGRAM_ENDPOINT = "?__a=1";
-    public static final String INSTAGRAM_HD_PIC_ENDPOINT = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/profile";
+    public static final String INSTAGRAM_HD_PIC_ENDPOINT = "https://instagram-unofficial-api.herokuapp.com/unofficial/api/profile";
     public static final String INSTAGRAM_USER_ID = "?userid=";
-    public static final String INSTAGRAM_VIDEOS_ENDPOINT = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/video";
+    public static final String INSTAGRAM_VIDEOS_ENDPOINT = "https://instagram-unofficial-api.herokuapp.com/unofficial/api/video";
     public static final String INSTAGRAM_VIDEO_PATH = "?link=";
-    public static final String INSTAGRAM_SEARCH_USER = "http://instagramapi-env.eba-k3fdhyr7.ap-south-1.elasticbeanstalk.com/unofficial/api/topsearch";
+    public static final String INSTAGRAM_SEARCH_USER = "https://instagram-unofficial-api.herokuapp.com/unofficial/api/topsearch";
     public static final String INSTAGRAM_QUERY_USER = "?query=";
 }
