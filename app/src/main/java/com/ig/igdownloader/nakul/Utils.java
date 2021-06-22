1b16c93f03ff2bb6a9c70ccccc0fcb0dfa54d4f0
Nakul Singh <NakulS426@gmail.com>
2021-06-22 13:30:35 +0530

    Remove unused function

--- app/src/main/java/com/ig/igdownloader/nakul/Utils.java
+++ app/src/main/java/com/ig/igdownloader/nakul/Utils.java
@@ -72,35 +72,12 @@ public class Utils extends Activity {
     public static void displaySize(WindowManager windowManager, AlertDialog alertDialog) {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        /*int displayWidth = displayMetrics.widthPixels;
-        int displayHeight = displayMetrics.heightPixels;*/
+        
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
         lp.copyFrom(Objects.requireNonNull(alertDialog.getWindow()).getAttributes());
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
         alertDialog.getWindow().setAttributes(lp);
-
-        /*Log.d(TAG, "displaySize: "+displayWidth +"x"+ displayHeight);
-        
-        if (displayWidth <= 1080 && displayHeight <= 1772) {
-            Log.d(TAG, "displaySize: executing first if");
-            int dialogWidth = (int) (displayWidth * 0.9F);
-            int dialogHeight = (int) (displayHeight * 0.38F);
-            lp.width = dialogWidth;
-            lp.height = dialogHeight;
-            alertDialog.getWindow().setAttributes(lp);
-        } else if (displayWidth <=1080 && displayHeight >=2030) {
-            Log.d(TAG, "displaySize: executing second if");
-            int dialogWidth = (int) (displayWidth * 0.9F); // 972
-            int dialogHeight = (int) (displayHeight * 0.29F); // 568
-            lp.width = dialogWidth;
-            lp.height = dialogHeight;
-            alertDialog.getWindow().setAttributes(lp);
-            Log.d(TAG,dialogWidth +"x" +dialogHeight);
-        }
-        // Set the dialog height and width.
-        int dialogWidth = (int) (displayWidth * 0.9F);
-        int dialogHeight = (int) (displayHeight * 0.28F);*/
 
     }
 }
